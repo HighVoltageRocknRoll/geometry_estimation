ef279c55cb1a872ed6592c7944bbd09463cb3ff7
Константин Кожемяков <kostonz@yandex.ru>
2020-02-23 23:01:10 +0300

    fixed detaching tensors in metric calculation

--- util/eval_util.py
+++ util/eval_util.py
@@ -152,9 +152,9 @@ def absdiff_metrics(batch,batch_start_idx,theta_1,theta_2,geometric_model_1,geom
     current_batch_size=batch['source_im_size'].size(0)
     indices = range(batch_start_idx,batch_start_idx+current_batch_size)
 
-    stats[geometric_model_1]['rotate_diff'][indices] = abs((affine_simple_values[:, 0] - theta_1[:, 0]).cpu().numpy())
-    stats[geometric_model_1]['scale_diff'][indices] = abs((affine_simple_values[:, 1] - theta_1[:, 1]).cpu().numpy())
-    stats[geometric_model_1]['shift_diff'][indices] = abs((affine_simple_values[:, 2] - theta_1[:, 2]).cpu().numpy())
+    stats[geometric_model_1]['rotate_diff'][indices] = abs((affine_simple_values[:, 0] - theta_1[:, 0]).detach().cpu().numpy())
+    stats[geometric_model_1]['scale_diff'][indices] = abs((affine_simple_values[:, 1] - theta_1[:, 1]).detach().cpu().numpy())
+    stats[geometric_model_1]['shift_diff'][indices] = abs((affine_simple_values[:, 2] - theta_1[:, 2]).detach().cpu().numpy())
         
     return stats
 
